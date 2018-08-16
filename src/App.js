5f3436c3b8ca784a31019ccbaa8bee632d2596ff
Arata Kagan <aratakagan@gmail.com>
2018-08-16 12:44:28 -0700

    Checkpoint event: finished

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
 
-
+//Parent component and set initial state
+//Pass down the state values to ToDo component
 class App extends Component {
   constructor(props){
     super(props);
@@ -11,18 +12,48 @@ class App extends Component {
         { description: 'Walk the dog', isCompleted: true },
         { description: 'Clean up kitchen', isCompleted: false},
         { description: "Buy ingredients for tonight's dinner", isCompleted: false }
-      ]
+      ],
+      newTodoDescription: ''
     }
   }
+
+  handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newTodoDescription) { return }
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false};
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+  }
+
+  //Event handler
+  toggleComplete(index) {
+    //make copy of array in order to modify the data
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    // revisit the structure of ternery function
+    todo.isCompleted = todo.isCompleted ? false : true;
+    // revisit why todos: todos
+    this.setState({ todos: todos });
+  }
+
 
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-              <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+              // Pass anonymous function to toggleComplete
+              // Index allows us to select appropriate todo
+              <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
           )}
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type='text' value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class ToDo extends Component {
-  render() {
+  render(){
     return (
       <li>
-        <input type='checkbox' checked={ this.props.isCompleted } />
-        <span>{ this.props.description }</span>
+        <input type='checkbox' checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+        <span>{this.props.description}</span>
       </li>
     );
   }
